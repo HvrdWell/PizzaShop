0676256b7a69add11f1a11df26ce5fdbef891704
HvrdWell <geka23111@gmail.com>
2023-01-04 02:17:54 +0300

    update cart

--- PizzaShop/ViewModel/CartViewModel.swift
+++ PizzaShop/ViewModel/CartViewModel.swift
@@ -10,7 +10,13 @@ import Foundation
 class CartViewModel : ObservableObject {
     
     @Published var positions = [Position]( )
-    
+    var costTotal: Int{
+        var sum = 0
+        for pos in positions{
+            sum += pos.cost
+        }
+        return sum
+    }
     func addPosition(_ position: Position){
         self.positions.append(position)
     }
--- PizzaShop/Views/CartView.swift
+++ PizzaShop/Views/CartView.swift
@@ -11,7 +11,49 @@ struct CartView: View {
     var viewModel: CartViewModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            List(viewModel.positions){ position in
+                PositionCell(position: position)
+            }.listStyle(.plain)
+                .navigationTitle("Корзина")
+            HStack{
+                Text("Итого:")
+                    .fontWeight(.bold)
+                Spacer( )
+                Text("\(self.viewModel.costTotal) ₽")
+                    .fontWeight(.bold)
+            }.padding()
+            
+            HStack(spacing: 24){
+                Button {
+                    print("cancel")
+                } label: {
+                    Text("Отменить")
+                        .font(.body)
+                        .fontWeight(.bold)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.red)
+                        .cornerRadius(15)
+                }
+                Button {
+                    print("add")
+                } label: {
+                    Text("Заказать")
+                        .font(.body)
+                        .fontWeight(.bold)
+                        .padding()
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.green)
+                        .cornerRadius(15)
+                }
+            }
+            
+        }.padding()
+      
+            
+        
     }
 }
 
