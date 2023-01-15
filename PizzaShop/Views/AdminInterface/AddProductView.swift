3267316fdd49d488a9351fcd71a9c99d38776933
HvrdWell <geka23111@gmail.com>
2023-01-16 00:41:00 +0300

    admin panel/adding a product/interface(without binding to firebase)

--- PizzaShop/Views/AdminInterface/AddProductView.swift
+++ PizzaShop/Views/AdminInterface/AddProductView.swift
@@ -9,19 +9,48 @@ import SwiftUI
 
 struct AddProductView: View {
     @State private var showImagePicker = false
-    @State private var image = UIImage( )
+    @State private var image = UIImage(named: "margarita" )!
+    @State private var title: String = ""
+    @State private var descript: String = ""
+    @State private var price: Int? = nil
+
     var body: some View {
         VStack{
-            Image("margarita")
+            Text("Добавить товар")
+                .font(.title2)
+                .bold()
+            Image(uiImage: image)
                 .resizable()
                 .scaledToFill()
-                .frame(maxWidth: .infinity, maxHeight: 200)
+                .frame(maxWidth: .infinity, maxHeight: 250)
                 .clipped()
                 .cornerRadius(5)
                 .onTapGesture {
                     showImagePicker.toggle()
                 }
+            
+            TextField("Название продукта", text: $title)
+                .padding()
+            TextField("Цена продукта", value: $price, format: .number)
+                .keyboardType(.numberPad)
+                .padding()
+            TextField("Описание продукта", text: $descript)
+                .padding()
+            Button {
+                print("Cохранить")
+            } label: {
+                Text("Cохранить")
+                    .padding()
+                    .padding(.horizontal,30)
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(16)
+            }
+
+            Spacer()
+            
         }
+        .padding()
         .sheet(isPresented: $showImagePicker) {
             ImagePicker(sourceType: .photoLibrary, selectedImage: $image)
         }
